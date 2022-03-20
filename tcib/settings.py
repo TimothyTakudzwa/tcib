5150add6a924dfa5c85fe688b6d76e390afabf83
Timoth Ngorima <timothytakudzwa@gmail.com>
2022-03-20 22:09:25 +0200

    Transaction

--- tcib/settings.py
+++ tcib/settings.py
@@ -108,8 +108,12 @@ AUTH_USER_MODEL = 'api.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'tcib',
+        'USER': 'tcib',
+        'PASSWORD': 'tcib1234#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
